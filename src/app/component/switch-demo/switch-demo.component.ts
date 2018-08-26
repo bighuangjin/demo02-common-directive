02320980ff15f20dfea73b0d1472566aafd92d44
BertHuang <bert.j.huang@pwc.com>
2018-08-26 23:01:56 +0800

    add the ng switch-case-default component

---
+++ src/app/component/switch-demo/switch-demo.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-demo',
+  templateUrl: './switch-demo.component.html',
+  styleUrls: ['./switch-demo.component.css']
+})
+export class SwitchDemoComponent implements OnInit {
+  condition = 1;
+  constructor() {}
+
+  ngOnInit() {}
+}
